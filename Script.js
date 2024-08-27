1f2832031182b16517f62b6efceca090bd6461d4
Kurtmeisterino <kurdo_rodin@hotmail.com>
2024-08-27 23:36:26 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,2 @@
+(function(c,l,i,e,n,t,o){if(c.cbk){return;}c.cbk=function(){ c.cbk.p.push(arguments);};c.cbk.p=[];e=l.createElement("script");e.async=1; e.src=i;l.head.appendChild(e);n=l.createElement("div");n.id="cliento-booking"; t=l.getElementsByTagName("script");o=t[t.length-1];o.parentNode.insertBefore(n,o); })(window,document,"https://cliento.com/widget-v2/cliento.js");
+cbk("id", "7G9QqThMnk4oPoJ4hVlY8v");
